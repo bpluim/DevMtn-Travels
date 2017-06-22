006e0d7f3200e6bb27058db5520a478df9a0abdb
Ben Pluim <benpluim@Bens-MacBook-Pro.local>
2017-06-21 20:46:51 -0600

    UI-Routing Practice

--- js/app.js
+++ js/app.js
@@ -4,7 +4,37 @@ angular.module('devmtnTravel', ['ui.router'])
             .state('home',{
                 url:'/',
                 templateUrl: "../views/about.html"
-            });
+            })
+            .state('adventurers', {
+                url: '/adventurers',
+                // parent: 'home',
+                templateUrl: '../views/about-adventurers.html'
+            })
+            .state('contact', {
+                url: '/contact',
+                // parent: 'home',
+                templateUrl: '../views/contact.html'
+            })
+            .state('allPackages', {
+                url: '/allPackages',
+                templateUrl: '../views/packagesAll.html',
+                controller: 'packagesAllCtrl'
+            })
+            .state('packagesCountry', {
+                url: '/packages/:country',
+                templateUrl: '../views/packagesCountry.html',
+                controller: 'packagesCtrl'
+            })
+            .state('booked', {
+                url: '/booked/:id',
+                templateUrl: '../views/booked.html',
+                controller: 'bookedCtrl'
+            })
+            .state('locations', {
+                url: '/locations',
+                templateUrl: "../views/locations.html",
+                controller: 'locationsCtrl'
+            })
 
         $urlRouterProvider
             .otherwise('/');
